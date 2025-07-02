7f1bb9815691eacf775127bb27630832d7e6785b
DonMischo <micha.poehle@gmail.com>
2025-07-02 13:17:01 +0200

    new template
    clean
    lua for testing

---
+++ Test/New/studentdata.lua
@@ -0,0 +1,71 @@
+student = {
+    first_name = "Max",
+    last_name = "Mustermann",
+    classRoom = "5c",
+    date_of_birth = "29.12.2013",
+    school_year = "2023/2024",
+	part_of_year = "Schuljahr",
+    location = "Erfurt",
+    report_date = "19.06.2024",
+    personal_text = [[
+        Dein erstes Schuljahr an der Evangelischen Gemeinschaftsschule geht zu Ende. Du hast dich gut eingelebt und tolle Fortschritte gemacht.
+    ]],
+    participation = 2,
+    behavior = 1,
+    absenceDaysTotal = 6,
+    absenceDaysUnauthorized = 0,
+	absenceHoursTotal = 0,
+	absenceHoursUnauthorized = 0,
+	comment = "Zweitausfertigung",
+    subjects = {
+        {
+            name = "Deutsch",
+            topics = {
+                {
+                    title = "Lese- / Hörverstehen",
+                    competences = {
+                        { description = "Ich kann Texte verstehen."},
+                        { description = "Ich kann Texte vorlesen."},
+                    },
+					grade = 2,
+                },
+                {
+                    title = "Sprechen",
+                    competences = {
+                        { description = "Ich kann ein Gespräch führen."}
+                    },
+					grade = "nb",
+                }
+            },
+			level = 1,			
+        },
+        {
+            name = "Mathematik",
+            topics = {
+                {
+                    title = "Arithmetik",
+                    competences = {
+                        { description = "Ich kann Zahlen runden."},
+                        { description = "Ich kann Terme umformen."}
+                    },
+					grade = "HJ2",
+                }
+            },
+			level = 3,
+        },
+		{
+            name = "Englisch",
+            topics = {
+                {
+                    title = "Sprache",
+                    competences = {
+                        { description = "Ich kann Zahlen runden."},
+                        { description = "Ich kann Terme umformen."}
+                    },
+					grade = 3,
+                },
+            },
+			level = "WTF have u done",
+        }
+    }
+}
